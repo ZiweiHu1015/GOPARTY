9d1fbee11bb6352bb184eb2ab962c90c2db236e8
ZiweiHu1015 <36269090+ZiweiHu1015@users.noreply.github.com>
2024-04-13 22:08:47 -0400

    gigs page

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -4,7 +4,29 @@ import "./Gig.scss"
 const Gig = () => {
   return (
     <div className = "Gig">
-      Gig
+      <div className = "container">
+        <div className = "left">
+            <span className="breadCrumbs"> GoParty >Graphics & Design ></span>
+            <h1>I will deliver balloons for you</h1>
+
+            <div className = "user">
+                <img src="" alt="" />
+                <span>Emma Hu</span>
+                <div className = "stars"> 
+                    <img src="../img/star.png" alt="" />
+                    <img src="../img/star.png" alt="" />
+                    <img src="../img/star.png" alt="" />
+                    <img src="../img/star.png" alt="" />
+                    <img src="../img/star.png" alt="" />
+                    <span>5</span>
+
+                </div>
+
+            </div>
+        </div>
+        <div className = "right"></div>
+
+      </div>
     </div>
   )
 }
--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
-import "./Gigs.scss"
+import React, { useEffect, useRef, useState } from "react";
+import "./Gigs.scss";
+import GigCard from "../../components/gigCard/GigCard";
+import { useLocation } from "react-router-dom";
+import {gigs} from "../../data";
 
 const Gigs = () => {
+    const [sort, setSort] = useState("sales");
+    const [open, setOpen] = useState(false);
+    const minRef = useRef();
+    const maxRef = useRef();
+
+    const reSort = (type) =>{
+        setSort(type)
+        setOpen(false)
+    }
+
+    const { search } = useLocation();
+
   return (
-    <div className = 'Gigs'>
-      Gig
+    <div className = 'gigs'>
+      <div className = "container">
+        <span className = "breadcrumbs"> GoParty  Graphics & Design</span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Liverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder="min"/>
+            <input type="text" placeholder="max"/>
+            <button>Apply</button>
+           </div>
+           <div className="right">
+            <span className = "sortBy"> SortBy</span>
+            <span className = "sortType">{sort === "sales"? "Best Selling" : "Newest"} </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open &&(
+                <div className="rightMenu">
+                    {sort === "sales" ? (
+                        <span onClick={() => reSort("createdAt")}>Newest</span>
+                    ) : (
+                        <span onClick={() => reSort("sales")}>Best Selling</span>
+                    )}
+                </div>
+            )}
+                </div>
+            </div>
+            <div className = "cards">
+                {gigs.map(gig=>(
+                    <GigCard key={gig._id} item={gig} />
+
+                ))
+
+                }
+
+
+            </div>
+        </div>
     </div>
+
+ 
+  
   )
 }
 
