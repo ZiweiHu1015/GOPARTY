00063f5b044b6efa149fbcc6e47d7a8012c9f203
ZiweiHu1015 <36269090+ZiweiHu1015@users.noreply.github.com>
2024-04-15 00:09:23 -0400

    finsh gigs pages

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,138 +1,139 @@
 import React from 'react'
 import { Slider } from "infinite-react-carousel/lib";
-import "./Gig.scss"
+import "./Gig.scss";
 
 const Gig = () => {
   return (
-    <div className = "Gig">
-      <div className = "container">
-        <div className = "left">
-            <span className="breadCrumbs"> GoParty {">"} Graphics & Design {">"}</span>
-            <h1>I will deliver balloons for you</h1>
+   <div className="gig">
+    <div className="container">
+      <div className="left">
+        
+        <span className="breadCrum"> GoParty {">"}  Graphics & Design {">"} </span>
+        <h1>I will deliver balloon for you</h1>
 
-            <div className = "user">
-                <img src="https://i.pinimg.com/564x/d4/5e/c7/d45ec76edd768fb26a788627261ff37e.jpg" alt="" />
-                <span>Emma Hu</span>
-                <div className = "stars"> 
-                    <img src="../img/star.png" alt="" />
-                    <img src="../img/star.png" alt="" />
-                    <img src="../img/star.png" alt="" />
-                    <img src="../img/star.png" alt="" />
-                    <img src="../img/star.png" alt="" />
-                    <span>5</span>
+        <div className="user">
+          <img 
+                className = "pp" 
+                src="../img/userProfile.jpg" 
+                alt="" 
+          />
+          <span>Joe Doe</span>
+          <div className="stars">
+            <img src="../img/star.png" alt="" />
+            <img src="/img/star.png" alt="" />
+            <img src="/img/star.png" alt="" />
+            <img src="/img/star.png" alt="" />
+            <img src="/img/star.png" alt="" />
+            <span>5</span>
+          </div>
+        </div>
+
+        <Slider slidesToShow={1} arrowsScrolll={1} className = "slider">
+          <img src="/img/placeholder.jpg" alt="" />
+          <img src="/img/placeholder.jpg" alt="" />
+          <img src="/img/placeholder.jpg" alt="" />
+        </Slider>
+
+        <h2>About this decoration</h2>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+        officia deserunt mollit anim id est laborum.
+        </p>
+
+
+        <div className="seller">
+            <h2>About the Seller</h2>
+            <div className="user">
+              <img src="../img/sellerProfile.jpg" alt="" />
+              <div className="info">
+                <span>Joey</span>
+                <div className="stars">
+                  <img src="../img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
                 </div>
-            </div>
-            <Slider slidesToShow={1} arrowsScroll = {1}>
-              <img src="../img/placeholder.JPG" alt="" />
-              <img src="" alt="" />
-              <img src="" alt="" />
-            </Slider>
-            <h2>About This package</h2>
-            <p>
-            Although Scott said it didn't matter to him, he knew deep inside that it did. 
-            They had been friends as long as he could remember and not once had he had to 
-            protest that something Joe apologized for doing didn't really matter. Scott 
-            stuck to his lie and insisted again and again that everything was fine as Joe 
-            continued to apologize. Scott already knew that despite his words accepting 
-            the apologies that their friendship would never be the same.
-            </p>
-            <div className = "seller">
-              <h2>About The Seller</h2>
-              <div className="user">
-                <img src="https://i.pinimg.com/564x/d4/5e/c7/d45ec76edd768fb26a788627261ff37e.jpg" alt="" />
-                <div className="info">
-                  <span>Emma Hu</span>
-                    <div className = "stars"> 
-                      <img src="../img/star.png" alt="" />
-                      <img src="../img/star.png" alt="" />
-                      <img src="../img/star.png" alt="" />
-                      <img src="../img/star.png" alt="" />
-                      <img src="../img/star.png" alt="" />
-                      <span>5</span>
-                  </div>
-                  <button>
-                    Contact Me
-                  </button>
-                </div>
-              </div>
-              <div className="box">
-                <div className="items">
-                  <div className="item">
-                    <span className="title">From</span>
-                    <span className="title">USA</span>
-                  </div>
-                  
-                  <div className="item">
-                    <span className="title">From</span>
-                    <span className="title">USA</span>
-                  </div>
-
-                  <div className="item">
-                    <span className="title">From</span>
-                    <span className="title">USA</span>
-                  </div>
-
-                  <div className="item">
-                    <span className="title">From</span>
-                    <span className="title">USA</span>
-                  </div>
-
-                  <div className="item">
-                    <span className="title">From</span>
-                    <span className="title">USA</span>
-                  </div>
-                </div>
-                  <p>
-                    I'm a good event planner 
-                  </p>
+                <button>Contact Me</button>
               </div>
             </div>
-            <div className="review">
-              <h2>Review</h2>
-              <div className="item">
-                <div className="user">
-                  <img src="../img/usflag.png" alt="" />
-                  <div className="info">
-                    <span> Joe Biden</span>
-                    <div className="country">
-                      <img src="../img/usflag.png" alt="" />
-                      <span>United States</span>
-                    </div>
-                  </div>
+            <div className="box">
+              <div className="items">
+               <div className="item">
+                  <span className="title">From</span>
+                  <span className="desc">USA</span>
                 </div>
-
-                <div className = "stars"> 
-                    <img src="../img/star.png" alt="" />
-                    <img src="../img/star.png" alt="" />
-                    <img src="../img/star.png" alt="" />
-                    <img src="../img/star.png" alt="" />
-                    <img src="../img/star.png" alt="" />
-                    <span>5</span>
+                <div className="item">
+                  <span className="title">Member since</span>
+                  <span className="desc">Aug 2022</span>
                 </div>
-
-                <p>
-                Although Scott said it didn't matter to him, he knew deep inside that it did. 
-                They had been friends as long as he could remember and not once had he had to 
-                protest that something Joe apologized for doing didn't really matter. Scott 
-                stuck to his lie and insisted again and again that everything was fine as Joe 
-                continued to apologize. Scott already knew that despite his words accepting 
-                the apologies that their friendship would never be the same.
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="./img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="./img/dislike.png" alt="" />
-                  <span>No</span>
+                <div className="item">
+                  <span className="title">Ave.response time</span>
+                  <span className="desc">4 hours</span>
+                </div>
+                <div className="item">
+                  <span className="title">Last delivery</span>
+                  <span className="desc">1 day</span>
+                </div>
+                <div className="item">
+                  <span className="title">Languages</span>
+                  <span className="desc">English</span>
                 </div>
               </div>
-              <hr/>
+              <hr />
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+              dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+              officia deserunt mollit anim id est laborum.
+              </p>
+          </div>
+        </div>
+       
+      </div>
+      <div className="right">
+        <div className="price">
+          <h3>1 AI generated img</h3>
+          <h2>$59.99</h2>
+        </div>
+        <p>I will create unique balloons</p>
+        <div className="details">
+          <div className="item">
+            <img src="../img/clock.png" alt="" />
+            <span>2 days delivery</span>
+          </div>
+
+          <div className="item">
+            <img src="../img/recycle.png" alt="" />
+            <span>3 Revisions</span>
+          </div>
+        </div>
+        <div className="features">
+            <div className="item">
+              <img src="../img/greencheck.png" alt="" />
+              <span>Promt writing</span>
+            </div>
+
+            <div className="item">
+              <img src="../img/greencheck.png" alt="" />
+              <span>Promt writing</span>
+            </div>
+
+            <div className="item">
+              <img src="../img/greencheck.png" alt="" />
+              <span>Promt writing</span>
             </div>
         </div>
-        <div className = "right"></div>
+        <button>Continue</button>
 
       </div>
     </div>
+   </div>
   )
 }
 
