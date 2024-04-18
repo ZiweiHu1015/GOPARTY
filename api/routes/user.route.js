94e094dc6e8189c30cf617d72f61a454a0b32661
ZiweiHu1015 <36269090+ZiweiHu1015@users.noreply.github.com>
2024-04-17 21:30:21 -0400

    routes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,9 @@
 import express from "express";
 
+
 const router = express.Router();
 
+router.get("/register")
+router.get("/login", )
 
 export default router;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -18,7 +25,20 @@ const connect = async () => {
     }
   };
 
-  app.use("/api/user", userRoute);
+  app.use(express.json());
+
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+  app.use("/api/gigs", gigRoute);
+  app.use("/api/orders", orderRoute);
+  app.use("/api/conversations", conversationRoute);
+  app.use("/api/messages", messageRoute);
+  app.use("/api/reviews", reviewRoute);
+  
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
 
   app.listen(8800, () =>{
     connect()
