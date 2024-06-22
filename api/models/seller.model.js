0009167b76caccba4072f93b47abcf1a5e886003
70mekng <54061249+70mekng@users.noreply.github.com>
2024-06-22 16:14:39 -0400

    Delete api directory

--- api/models/seller.model.js
+++
@@ -1,9 +0,0 @@
-// Assuming you have a similar setup for interacting with your database
-import db from '../database.js'; 
-
-export const createSeller = async (sellerData) => {
-  const { UserID, StoreName, StoreDescription, MainService, ServiceDays, ServiceArea, ServiceType } = sellerData;
-  const sql = `INSERT INTO Sellers (UserID, StoreName, StoreDescription, MainService, ServiceDays, ServiceArea, ServiceType)
-               VALUES (?, ?, ?, ?, ?, ?, ?)`;
-  await db.execute(sql, [UserID, StoreName, StoreDescription, MainService, ServiceDays, ServiceArea, ServiceType]);
-};
