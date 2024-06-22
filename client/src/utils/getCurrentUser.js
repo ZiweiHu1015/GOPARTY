f0d0c7a067a850e670757d373dfcdd1d2894112c
70mekng <54061249+70mekng@users.noreply.github.com>
2024-06-22 16:14:31 -0400

    Delete client directory

--- client/src/utils/getCurrentUser.js
+++
@@ -1,9 +0,0 @@
-const getCurrentUser = () => {
-
-  const userString = localStorage.getItem("currentUser");
-
-  //console.log(userString);
-  return JSON.parse(localStorage.getItem("currentUser"));
-};
-
-export default getCurrentUser
